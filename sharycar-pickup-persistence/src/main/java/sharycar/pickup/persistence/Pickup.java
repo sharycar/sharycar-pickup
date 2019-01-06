3128c2efaf1de54c2fbdfb210be5cac73feca4e6
Janez Skrlj <janez321@gmail.com>
2019-01-06 16:52:22 +0100

    pickup persistence

---
+++ sharycar-pickup-persistence/src/main/java/sharycar/pickup/persistence/Pickup.java
@@ -0,0 +1,73 @@
+package sharycar.pickup.persistence;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "pickups")
+@NamedQuery(name = "Pickup.findAll", query = "SELECT p FROM Pickup p")
+public class Pickup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String pickUpLocation;
+
+    private String returnLocation;
+
+    private Integer car_id;
+
+    public Integer getCar_id() {
+        return car_id;
+    }
+
+    public void setCar_id(Integer car_id) {
+        this.car_id = car_id;
+    }
+
+    private Date pickupDateTime;
+
+    private Date returnDateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPickUpLocation() {
+        return pickUpLocation;
+    }
+
+    public void setPickUpLocation(String pickUpLocation) {
+        this.pickUpLocation = pickUpLocation;
+    }
+
+    public String getReturnLocation() {
+        return returnLocation;
+    }
+
+    public void setReturnLocation(String returnLocation) {
+        this.returnLocation = returnLocation;
+    }
+
+    public Date getPickupDateTime() {
+        return pickupDateTime;
+    }
+
+    public void setPickupDateTime(Date pickupDateTime) {
+        this.pickupDateTime = pickupDateTime;
+    }
+
+    public Date getReturnDateTime() {
+        return returnDateTime;
+    }
+
+    public void setReturnDateTime(Date returnDateTime) {
+        this.returnDateTime = returnDateTime;
+    }
+}
+
